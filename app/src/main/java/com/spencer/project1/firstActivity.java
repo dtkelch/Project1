d801183ceac15f93d9199ec4b6c66ac05c0a9f77
Spencer Amann <amanns@mail.gvsu.edu>
2015-05-19 15:08:21 -0400

    first commit

---
+++ app/src/main/java/com/spencer/project1/firstActivity.java
@@ -0,0 +1,63 @@
+package com.spencer.project1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_first)
+public class firstActivity extends ActionBarActivity {
+
+    @Extra
+    String testString;
+
+    @ViewById(R.id.textView)
+    TextView textView;
+
+    @AfterViews
+    void afterView() {
+        textView.setText(testString);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Click(R.id.button)
+    void buttonWasClicked() {
+        System.out.println("success");
+
+        secondActivity_.intent(this)
+                .testString("hello there second activity")
+                .start();
+    }
+}
---
+++ app/src/test/java/com/spencer/project1/ExampleTest.java
@@ -0,0 +1,15 @@
+package com.spencer.project1;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by amanns on 5/19/15.
+ */
+public class ExampleTest {
+    @Test
+    public void textExample() {
+        assertTrue("One should be one :D", 1 == 1);
+    }
+}
